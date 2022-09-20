49394f0b9495f3bfc5bcf99b107e86b258fb0536
kisnak21 <krisnastya21@gmail.com>
2022-09-20 19:31:37 +0700

    styling dan google font

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import 'regenerator-runtime';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/js/dist/modal';
 import './styles/style.css';
-
+// import './script/component/app-bar.js';
 import main from './view/main';
 
 document.addEventListener('DOMContentLoaded', main);
